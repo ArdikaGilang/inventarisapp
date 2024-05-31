15930868024d382da0f35d595791217684773d22
Gilang Ardika <ardikagilang718@gmail.com>
2024-05-31 14:29:36 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+      <h3>{{ $title }}</h3>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{{ $isi }}</h5>
+          </div>
+        </div>
+    </div>
+</div>
+@endsection
